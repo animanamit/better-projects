9656b91d06edd002578bade1655d7755924de8dd
Animan Amit <36138723+animanamit@users.noreply.github.com>
2025-04-15 21:35:23 +0800

    fix frontend build

--- frontend/src/lib/trpc.ts
+++ frontend/src/lib/trpc.ts
@@ -7,19 +7,7 @@ export const trpc = createTRPCReact<AppRouter>();
 export const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
-      url: "http://localhost:3000/trpc",
-      fetch(url, options) {
-        return fetch(url, {
-          ...options,
-          // DO NOT ADD CONTENT TYPE HEADER, SOME ERROR WITH TRPC CURRENTLY
-        }).then(async (response) => {
-          if (!response.ok) {
-            const error = await response.json().catch(() => ({}));
-            throw new Error(error.message || "Something went wrong");
-          }
-          return response;
-        });
-      },
+      url: import.meta.env.VITE_BACKEND_URL + "/trpc",
     }),
   ],
 });
